ccee9f88f76eeeef188d2d708265f2cb376aad82
Marcin Swornowski <swor71@gmail.com>
2018-12-05 22:41:59 +0100

    Add basics of working with files, creating, writing, appending, reading

--- files.py
+++ files.py
@@ -1,1 +1,28 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+
+myFile = open('myfile.txt', 'w')  # w - write mode
+
+# Get some info
+
+print('name: ', myFile.name)
+print('is opened: ', myFile.closed)
+print('opening mode: ', myFile.mode)
+
+# Write to file
+
+myFile.write('Jeg elsker Python')
+myFile.write(' og JavaScript,')
+myFile.close()
+
+# Append to a file
+
+myFile = open('myfile.txt', 'a')  # a - append mode
+myFile.write(' Jeg liker React ogsa')
+
+# Read from file
+
+myFile = open('myfile.txt', 'r+')  # r+ - read mode
+text = myFile.read(100)
+print(text)
