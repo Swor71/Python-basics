82ae5c6312924113d11a3d24becc7a41a363fced
Marcin Swornowski <swor71@gmail.com>
2018-12-05 18:37:31 +0100

    Add basics of tuples and sets

--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,42 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# Create a tuple
+
+fruits = ('Apples', 'Oranges', 'Grapes')
+fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+# Single value needs trailing comma!!
+
+fruits3 = ('Apples',)
+
+print(fruits[1])
+
+# Delete tuple
+
+print(len(fruits2))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+
+fruits_set = {'Apples', 'Orange', 'Mango'}
+
+# Check if something is in the set
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grape')
+print(fruits_set)
+
+# Remove
+
+fruits_set.remove('Grape')
+print(fruits_set)
+
+# Clear set
+
+fruits_set.clear()
+
+# Deleting
+
+del fruits_set
